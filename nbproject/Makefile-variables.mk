2e6903976466ffc18c712bace6b76906a90b3d34
jetibest <jeti2121@hotmail.com>
2021-06-16 16:54:18 +0200

    Added nbproject directory.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=libi2c-pic.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/libi2c-pic.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=libi2c-pic.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/libi2c-pic.tar
